d48a13f0f5cede9f535c0181a3dae23a79f3bcda
guillaume leclercq <guillaume.leclercq@fullsix.com>
2017-02-27 15:48:58 +0100

    mutation band

--- src/AppBundle/GraphQL/Mutation/Band/AddBandField.php
+++ src/AppBundle/GraphQL/Mutation/Band/AddBandField.php
@@ -2,7 +2,11 @@
 
 namespace AppBundle\GraphQL\Mutation\Band;
 
+use AppBundle\Entity\Band;
 use AppBundle\GraphQL\Type\BandType;
+use AppBundle\GraphQL\Type\BandTypeInput;
+use Proxies\__CG__\AppBundle\Entity\Style;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Youshido\GraphQL\Config\Field\FieldConfig;
 use Youshido\GraphQL\Execution\ResolveInfo;
 use Youshido\GraphQL\Type\AbstractType;
@@ -17,13 +21,28 @@ class AddBandField extends AbstractContainerAwareField
     public function build(FieldConfig $config)
     {
         $config->addArguments([
-            'name' => new NonNullType(new StringType()),
+            'band' => new NonNullType(new BandTypeInput()),
         ]);
     }
 
     public function resolve($value, array $args, ResolveInfo $info)
     {
-        return $this->container->get('resolver.band')->create($args['name']);
+        $em = $this->container->get('doctrine')->getManager();
+
+        $style = $em->getRepository(Style::class)->findOneById($args['band']['style']);
+
+        if (!$style) {
+            throw new Exception('No style found');
+        }
+
+        $band = new Band();
+        $band->setName($args['band']['name']);
+        $band->setStyle($style);
+
+        $em->persist($band);
+        $em->flush();
+
+        return $band;
     }
 
     /**
@@ -31,11 +50,11 @@ class AddBandField extends AbstractContainerAwareField
      */
     public function getType()
     {
-        return new ListType(new BandType());
+        return new BandType();
     }
 
     public function getName()
     {
-        return 'add';
+        return 'addBand';
     }
 }
---
+++ src/AppBundle/GraphQL/Type/BandTypeInput.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\GraphQL\Type;
+
+use Youshido\GraphQL\Config\Object\ObjectTypeConfig;
+use Youshido\GraphQL\Type\InputObject\AbstractInputObjectType;
+use Youshido\GraphQL\Type\Scalar\IdType;
+use Youshido\GraphQL\Type\Scalar\StringType;
+
+class BandTypeInput extends AbstractInputObjectType
+{
+    /**
+     * @param ObjectTypeConfig $config
+     *
+     * @return mixed
+     */
+    public function build($config)
+    {
+        $config->addFields([
+            'name' => new StringType(),
+            'style' => new IdType()
+        ]);
+    }
+}
